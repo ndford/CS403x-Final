de71a85f35a4aecbbd56d9b66e128c9d3df2eac1
David Mihal <eastsideski@gmail.com>
2015-04-29 17:05:26 -0400

    Create GeoPhoto Class

---
+++ app/src/main/java/com/davidmihal/geoimagestore/GeoPhoto.java
@@ -0,0 +1,63 @@
+package com.davidmihal.geoimagestore;
+
+import java.util.Date;
+
+/**
+ * Created by David Mihal on 4/29/2015.
+ */
+public class GeoPhoto {
+    private String name;
+    private Date date;
+    private String account;
+    private double latitude;
+    private double longitude;
+    private String fileKey;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getFileKey() {
+        return fileKey;
+    }
+
+    public void setFileKey(String fileKey) {
+        this.fileKey = fileKey;
+    }
+}
