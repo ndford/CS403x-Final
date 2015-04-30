e9dd0f771a0448aff0e1c2cd21c789c3683d1250
David Mihal <eastsideski@gmail.com>
2015-04-30 15:23:49 -0400

    Fix some error, not sure what this does

--- app/src/main/java/com/fbm/nkd/cs403x_final/FeedFragment.java
+++ app/src/main/java/com/fbm/nkd/cs403x_final/FeedFragment.java
@@ -30,8 +30,6 @@ public class FeedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
-        TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle1);
-        tvTitle.setText("Feed Fragment");
         return view;
     }
 }
