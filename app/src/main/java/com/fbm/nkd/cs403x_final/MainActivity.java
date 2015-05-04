6cef4205418332b5f73dd0a28557de36122ce018
Kristen Brann <kristenbrann@bitbucket.org>
2015-05-03 20:32:32 -0400

    Moved camera activity to be called from the Camera tab Got rid of the awkwardness Some awkwardness remains, such as: Photo orientation, and photo disappearing upon screen rotation.

--- app/src/main/java/com/fbm/nkd/cs403x_final/MainActivity.java
+++ app/src/main/java/com/fbm/nkd/cs403x_final/MainActivity.java
@@ -1,9 +1,18 @@
 package com.fbm.nkd.cs403x_final;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -13,18 +22,28 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
+import com.davidmihal.geoimagestore.GeoPhoto;
 
 
 public class MainActivity extends FragmentActivity {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQ = 0;
+    private static final String TAG = "CallCamera";
+    Uri fileUri = null;
+    ImageView photoImage = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +58,9 @@ public class MainActivity extends FragmentActivity {
         PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         // Attach the view pager to the tab strip
         tabsStrip.setViewPager(viewPager);
+
+
     }
-
-    public void takePicture(View view){
-        Intent intent = new Intent(this, CallCamera.class);
-        startActivity(intent);
-    }
-
-
-
 
 
 }
